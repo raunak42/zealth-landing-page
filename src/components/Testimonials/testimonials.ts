b984d7c1da5b04c7cae4ce0037372593152001f9
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-06-27 22:51:38 +0530

    Testimonial data.

---
+++ src/components/Testimonials/testimonials.ts
@@ -0,0 +1,41 @@
+export const patientTestimonials = [
+  {
+    name: "Mahesh",
+    title: "Caregiver of Stomach Cancer Patient",
+    testimony: "I have been tracking my mother-in-law’s symptoms since her treatments began. Careshare is a very useful app for me as it has helped me with keeping a track of her and I am very thankful for it as it has made monitoring so much more easier."
+    , image: "/fakeFace1.jpeg"
+  },
+  {
+    name: "Aditya",
+    title: "Lung cancer patient",
+    testimony: "I got instant answers to my burning questions because of Careshare. I didn't have to wait for hours to call hospitals. Their 24X7 support is amazing."
+    , image: "/fakeFace2.jpeg"
+  }, {
+    name: "Swati",
+    title: "Caregiver of oral cancer paitent",
+    testimony: "Careshare gave me peace of mind. We felt connected to the doctor all the time. I used to get instant replies from the doctor for all my questions."
+    , image: "/fakeFace3.jpeg"
+  }, {
+    name: "Mohan",
+    title: "Caregiver of blood cancer paitent",
+    testimony: "It was the 3rd day of using Careshare. My brother was alerted as high risk due to high ever severe vomiting. The hospital got an immediate alert and they informed us to take a Complete Blood Count. Careshare helped in saving my brother’s life."
+    , image: "/fakeFace4.jpeg"
+  },
+]
+
+export const doctorTestimonials = [
+  {
+    name: "Dr. Aarti Sinha",
+    title:"",
+    testimony: "CareShare has made patient follow up simple and effective. It has significantly increased patient engagement."
+    , image: "/fakeFace5.jpeg"
+  },
+  {
+    name: "Dr. Pawan Singh",
+    title:"",
+    testimony: "CareShare helped me get a 360 degree view of the patients health and status while at home. It has made monitoring the patient’s health very easy.",
+    image: "/fakeFace6.jpeg"
+  }
+]
+
+export const testimonials = patientTestimonials.concat(doctorTestimonials)
