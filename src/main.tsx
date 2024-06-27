111899690630bbb69173dc6aa0c56c5b7f200fb4
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-06-27 22:47:31 +0530

    Added browser-router, recoilroot.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RecoilRoot } from "recoil";
+// import { Parallax } from "@react-spring/parallax";
+import { Home } from "./pages/home.tsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <RecoilRoot>
+    <BrowserRouter>
+      <React.StrictMode>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </React.StrictMode>
+    </BrowserRouter>
+  </RecoilRoot>
+);
